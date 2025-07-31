09c7591287211c4328bba1e04f52c825a061c4aa
David Munoz <dmunoz@arrivelogistics.com>
2025-07-31 14:51:35 -0500

    Improved accessibility and code modularity

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { useEffect, useState } from "react";
 import { TagList } from "@/components/ui/tag-list";
 import { Header } from "@/components/ui/header";
 import { SearchBoxArea } from "@/components/ui/search-box-area";
-import { fetchTags } from "@/api/tags";
 
 export default function App() {
-  const [tags, setTags] = useState<string[]>([]);
-
-  useEffect(() => {
-    fetchTags().then((tags) => setTags(tags));
-  }, []);
-
   return (
     <main className="dark bg-background min-h-screen text-white">
       <Header title="Worctionary" />
       <SearchBoxArea />
-      <TagList title="Trending" tags={tags || []} />
-      <TagList title="For you" tags={tags || []} />
+      <TagList title="Trending" />
+      <TagList title="For you" />
     </main>
   );
 }
